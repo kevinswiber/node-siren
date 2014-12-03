d0bfdd4bcce6285453c51dee3733640e5dd5ebf5
Kevin Swiber <kswiber@gmail.com>
2014-12-03 14:10:42 -0500

    Updating revolt-json-parser and building support for actions.

--- example.js
+++ example.js
@@ -10,3 +10,18 @@ siren()
   .monitor()
   .subscribe(console.log);
 
+// Filtering vs Navigating
+//
+siren()
+  .load('http://zetta-cloud-2.herokuapp.com')
+  .link('http://rels.zettajs.io/peer', 'Detroit')
+  .entity(function(e) {
+    return e.properties.type === 'display';
+  })
+  .action('change', function(action) {
+    action.set('message', 'Hello world: ' + Date.now());
+    return action.submit();
+  })
+  .subscribe(function(env) {
+    console.log(env.response.statusCode);
+  });
--- siren.js
+++ siren.js
@@ -1,3 +1,5 @@
+var querystring = require('querystring');
+var url = require('url');
 var revolt = require('revolt');
 var jsonParser = require('revolt-json-parser');
 var Rx = require('rx');
@@ -55,6 +57,26 @@ Siren.prototype.entity = function(filter) {
   return this;
 };
 
+Siren.prototype.action = function(name, cb) {
+  var self = this;
+
+  this.current = this.current.flatMap(function(env) {
+    var entity = env.response.body;
+    if (entity.actions) {
+      var actions = entity.actions.filter(function(action) {
+        return action.name === name;
+      });
+
+      if (actions.length) {
+        var action = new Action(self.client, actions[0]);
+        return cb(action);
+      }
+    }
+  });
+
+  return this;
+};
+
 Siren.prototype.monitor = function() {
   this.current = this.current.flatMap(function(env) {
     return Rx.Observable.create(function(observer) {
@@ -77,3 +99,83 @@ Siren.prototype.subscribe = function(observer) {
   return this.current.subscribe(observer);
 };
 
+function Action(client, data) {
+  this._client = client;
+  this._data = data;
+};
+
+Action.prototype.available = function(name) {
+  var fields = this._data.fields.filter(function(field) {
+    return field.name === name;
+  })
+
+  return fields.length === 1;
+};
+
+Action.prototype.set = function(name, value) {
+  if (this._data.fields) {
+    var fields = this._data.fields.filter(function(field) {
+      return field.name === name;
+    })
+
+    if (fields.length === 1) {
+      fields[0].value = value;
+    }
+  }
+};
+
+Action.prototype.get = function(name) {
+  var fields = this._data.fields.filter(function(field) {
+    return field.name === name;
+  })
+
+  if (fields.length === 1) {
+    return fields[0];
+  }
+};
+
+Action.prototype.submit = function() {
+  var method = this._data.method || 'GET';
+  var href = this._data.href;
+  var type = this._data.type || 'application/x-www-form-urlencoded';
+  var body;
+
+  if (this._data.fields) {
+    var obj = {};
+    var actionData = this._data.fields.forEach(function(field) {
+      obj[field.name] = field.value;
+    });
+
+
+    if (method.toUpperCase() === 'GET') {
+      var parsed = url.parse(href);
+      var q = parsed.query || {};
+      Object.keys(obj).forEach(function(key) {
+        q[key] = obj[key];
+      });
+
+      parsed.query = q;
+
+      href = url.format(parsed);
+    } else {
+      if (type === 'application/x-www-form-urlencoded') {
+        body = querystring.stringify(obj);
+      } else if (type === 'application/json') {
+        body = JSON.stringify(obj);
+      } else {
+        body = new Buffer(obj);
+      }
+    }
+  }
+
+  var options = {
+    method: method,
+    uri: href,
+    headers: {
+      'Content-Type': type
+    },
+    body: body
+  };
+
+  return this._client.request(options);
+};
